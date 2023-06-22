770158eb84b4e1cf03a8bf7fe5e68ee4bce9b3ff
Maciej Czeszko <maciekc161@gmail.com>
2023-06-22 17:56:41 +0200

    Changed overlay in html and refactored javascript code so that it doesn't repeat itself, but uses functions

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const openModal = document.querySelector('#open-modal-btn');
 const closeModal = document.querySelector('#close-modal-btn');
 const modal = document.querySelector('.modal')
-const overlay = document.querySelector('#overlay')
+const overlay = document.querySelector('.overlay')
 
 openModal.addEventListener('click', () => {
     overlay.classList.remove('hide');
     modal.classList.remove('hide');
-    overlay.classList.add('overlay');
 })
 
 closeModal.addEventListener('click', () => {
+    overlay.classList.add('hide');
+    modal.classList.add('hide');
+})
+
+overlay.addEventListener('click', () => {
     overlay.classList.add('hide');
     modal.classList.add('hide');
 })
